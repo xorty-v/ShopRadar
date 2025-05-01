a270ecdea1adfd6944b70cd1754f15cc3fbb9fcb
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2025-05-01 20:32:12 +0400

    Update migration

--- src/ShopRadar.Infrastructure/Migrations/20250501001623_DomainDrivenDesign_Restructure.Designer.cs
+++ src/ShopRadar.Infrastructure/Migrations/20250501001623_DomainDrivenDesign_Restructure.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShopRadar.Infrastructure;
@@ -11,9 +12,11 @@ using ShopRadar.Infrastructure;
 namespace ShopRadar.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250501001623_DomainDrivenDesign_Restructure")]
+    partial class DomainDrivenDesign_Restructure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ShopRadar.Infrastructure/Migrations/20250501001623_DomainDrivenDesign_Restructure.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ShopRadar.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DomainDrivenDesign_Restructure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Stores_StoreId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_StoreId",
+                table: "Categories");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("28c7a7e6-fa41-4583-9661-56082b4a2144"));
+
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "Id",
+                keyValue: new Guid("bf167e4c-799a-4bdd-aa41-75ebabe9089b"));
+
+            migrationBuilder.DropColumn(
+                name: "DiscountPrice",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "StoreId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Stores",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Stores",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(35)",
+                oldMaxLength: 35);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.CreateTable(
+                name: "Offers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: true),
+                    StoreId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Url = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Offers_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Offers_Stores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Stores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PriceHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OfferId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    DiscountPrice = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    LastPriceOnUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PriceHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PriceHistory_Offers_OfferId",
+                        column: x => x.OfferId,
+                        principalTable: "Offers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("90f90635-ddfc-4e8b-83d6-b723e5e6a46f"), "Smartphones" },
+                    { new Guid("aab69d60-609c-41be-8bd0-e8b90d4c43fd"), "Keyboards" },
+                    { new Guid("bae1a401-84a9-4466-a8c3-aa466d3d3af0"), "Laptops" },
+                    { new Guid("c6b1741f-2c71-4bfc-9d9e-b87b9cafbf61"), "Headphones" },
+                    { new Guid("fc2f9d79-82b3-4305-a0b1-7341f99ba26c"), "Monitors" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Stores",
+                columns: new[] { "Id", "Name", "Url" },
+                values: new object[,]
+                {
+                    { new Guid("2509a6ea-b884-4f49-8b44-af7da9b7812c"), "Zoommer", "https://zoommer.ge/en" },
+                    { new Guid("8d779e8f-f6c5-46da-9eeb-5f749c7988ba"), "EliteElectronic", "https://ee.ge/" },
+                    { new Guid("99b698b7-ae46-4a2c-af66-d7155497caff"), "Alta", "https://alta.ge/?sl=en" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offers_ProductId",
+                table: "Offers",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offers_StoreId",
+                table: "Offers",
+                column: "StoreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PriceHistory_OfferId",
+                table: "PriceHistory",
+                column: "OfferId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PriceHistory");
+
+            migrationBuilder.DropTable(
+                name: "Offers");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("90f90635-ddfc-4e8b-83d6-b723e5e6a46f"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("aab69d60-609c-41be-8bd0-e8b90d4c43fd"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("bae1a401-84a9-4466-a8c3-aa466d3d3af0"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("c6b1741f-2c71-4bfc-9d9e-b87b9cafbf61"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("fc2f9d79-82b3-4305-a0b1-7341f99ba26c"));
+
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "Id",
+                keyValue: new Guid("2509a6ea-b884-4f49-8b44-af7da9b7812c"));
+
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "Id",
+                keyValue: new Guid("8d779e8f-f6c5-46da-9eeb-5f749c7988ba"));
+
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "Id",
+                keyValue: new Guid("99b698b7-ae46-4a2c-af66-d7155497caff"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Stores",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Stores",
+                type: "character varying(35)",
+                maxLength: 35,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountPrice",
+                table: "Products",
+                type: "numeric(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Products",
+                type: "character varying(350)",
+                maxLength: 350,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StoreId",
+                table: "Categories",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Categories",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "Stores",
+                columns: new[] { "Id", "Name", "Url" },
+                values: new object[] { new Guid("bf167e4c-799a-4bdd-aa41-75ebabe9089b"), "EliteElectronics", "https://ee.ge/" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name", "StoreId", "Url" },
+                values: new object[] { new Guid("28c7a7e6-fa41-4583-9661-56082b4a2144"), "Test Category", new Guid("bf167e4c-799a-4bdd-aa41-75ebabe9089b"), "TEST" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_StoreId",
+                table: "Categories",
+                column: "StoreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Stores_StoreId",
+                table: "Categories",
+                column: "StoreId",
+                principalTable: "Stores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
